1e42cadd21fb0760e744492dbad189de8d0cdb9e
sagar-das01 <sagarrdass@gmail.com>
2024-01-06 17:30:34 +0530

    Initializing spring backend and changing file structure

---
+++ movie-tracker-backend/src/main/java/com/example/movietracker/MovietrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.movietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovietrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovietrackerApplication.class, args);
+	}
+
+}
